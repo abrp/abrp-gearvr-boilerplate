6d79449edb000e849252a88b8cf38a80422df1c6
abrp <abrp@abrp.dk>
2016-04-08 08:58:43 +0200

    renaming of script
    change script name from changesize.cs to colorchanger.cs

--- Assets/Scripts/Abrp Utils/MoveableArea.cs
+++ Assets/Scripts/Abrp Utils/MoveableArea.cs
@@ -9,12 +9,18 @@ public class MoveableArea : MonoBehaviour {
 	private bool m_GazeOver;
 
 	private void Awake(){
-		m_InteractiveItem = gameObject.AddComponent<VRInteractiveItem> ();
+		if (m_InteractiveItem == null) {
+			m_InteractiveItem = gameObject.AddComponent<VRInteractiveItem> ();
+		}
 		m_Gaze = Camera.main.GetComponent<Gaze> ();
 	}
 
 	private void OnEnable(){
 		m_InteractiveItem.OnClick += HandleClick;
+	}
+
+	private void OnDisable(){
+		m_InteractiveItem.OnClick -= HandleClick;
 	}
 
 	private void HandleClick(){
--- Assets/Scripts/ColorChanger.cs
+++ Assets/Scripts/ColorChanger.cs
@@ -2,15 +2,21 @@
 using System.Collections;
 using VRStandardAssets.Utils;
 
-public class ChangeSize : MonoBehaviour {
+public class ColorChanger : MonoBehaviour {
 	[SerializeField] private VRInteractiveItem m_InteractiveItem;
 	[SerializeField] private Color m_OverColor = new Color(1,0,0);
 	[SerializeField] private Color m_OutColor = new Color(0,1,0);
-
+	private Renderer m_Renderer;
 	private bool m_GazeOver;
 
+	private void Start(){
+		m_Renderer = GetComponent<Renderer> ();
+	}
+
 	private void Awake(){
-		m_InteractiveItem = gameObject.AddComponent<VRInteractiveItem> ();
+		if (m_InteractiveItem == null) {
+			m_InteractiveItem = gameObject.AddComponent<VRInteractiveItem> ();
+		}
 	}
 
 	private void OnEnable(){
@@ -18,13 +24,18 @@ public class ChangeSize : MonoBehaviour {
 		m_InteractiveItem.OnOut += HandleOut;
 	}
 
+	private void OnEnable(){
+		m_InteractiveItem.OnOver -= HandleOver;
+		m_InteractiveItem.OnOut -= HandleOut;
+	}
+
 	private void HandleOver(){
-		GetComponent<Renderer> ().material.color = m_OverColor;
+		m_Renderer.material.color = m_OverColor;
 		m_GazeOver = true;
 	}
 
 	private void HandleOut(){
-		GetComponent<Renderer> ().material.color = m_OutColor;
+		m_Renderer.material.color = m_OutColor;
 		m_GazeOver = false;
 	}
 }
